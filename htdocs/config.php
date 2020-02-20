589bc06292e8755e9446a8bb1deb6336ce262b51
Martijn <122399@mborijnland.nl>
2020-02-20 12:38:06 +0100

    update
    config.php

--- htdocs/config.php
+++ htdocs/config.php
@@ -1,19 +1,16 @@
 <?php
 
-// ** MySQL settings - You can get this info from your web host ** //
+// MySQL settings 
 /** The name of the database for WordPress */
-define('DB_NAME', 'd');
+define('DB_NAME', 'localhost');
 /** MySQL database username */
-define('DB_USER', '');
+define('DB_USER', 'root');
 /** MySQL database password */
 define('DB_PASSWORD', '');
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'bakkerij');
 
 /** Title */
-define('SRV_NAME')
-
-
-
+define('SRV_NAME', 'BakkerijLeiden');
 
 ?>
