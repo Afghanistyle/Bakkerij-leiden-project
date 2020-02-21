9b8d5e5045a16dc277af0416aa31d278103207f0
Martijn <122399@mborijnland.nl>
2020-02-21 09:27:04 +0100

    update
    htdocs

---
+++ htdocs/footer.php
@@ -0,0 +1,25 @@
+  <!-- footer -->
+  <footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="grid_12">
+          <div class="socials">
+            <a href="#" class="fab fa-facebook"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-youtube"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+
+          </div>
+          <div class="copyright"><span class="brand">BakkerijLeiden </span> &copy; <span id="copyright-year"></span> |
+            <!-- <a href="#">Privacy Policy</a> --> Alle handelsmerken en logo's zijn eigendom van hun respectieve eigenaars.
+
+            <div>Website designed by <a href="http://www.mborijnland.nl" rel="nofollow">Martijn, Aron, Abulbari,
+                Bilal</a>
+            </div>
+            <div class="slashes"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
