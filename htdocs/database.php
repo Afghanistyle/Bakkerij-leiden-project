640b11ade5f25be05d13f7c20f934c18c0e68c8a
Martijn <122399@mborijnland.nl>
2020-03-13 10:31:06 +0100

    htdocs
    update

--- htdocs/database.php
+++ htdocs/database.php
@@ -1,48 +1,13 @@
-<?php
-session_start();
 
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'bakkerij_leiden';
 
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	
-	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+
+<?php 
+$con = mysqli_connect("localhost","root","","bakkerij_leiden");
+
+if(mysqli_connect_errno()){
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-// Now we check if the data from the login form was submitted, isset() will check if the data exists.
-if ( !isset($_POST['username'], $_POST['password']) ) {
-	// Could not get the data that should have been sent.
-	die ('niet werken lan!');
-}
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
 
-    $stmt->store_result();
-    
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $password);
-        $stmt->fetch();
-       
-        if (password_verify($_POST['password'], $password)) {
-          
-            session_regenerate_id();
-            $_SESSION['loggedin'] = TRUE;
-            $_SESSION['name'] = $_POST['username'];
-            $_SESSION['id'] = $id;
-            echo 'Welkom ' . $_SESSION['name'] . '!';
-        } else {
-            header('location: pu.php');
-        }
-    } else {
-        header('location: pu.php');
-    }
-
-	$stmt->close();
-}
+mysqli_query($con,"SET NAMES 'utf8' ");
 
 ?>
--- htdocs/login.php
+++ htdocs/login.php
@@ -1,3 +1,4 @@
+<?php include('database.php'); ?>
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -5,6 +6,8 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
     <link href="CSS/style-login.css" rel="stylesheet" type="text/css">
 	</head>
+
+
     <!-- login -->
 		<div class="login">
       <!-- <div id="shady"></div> -->
@@ -25,3 +28,40 @@
 		</div>
 	</body>
 </html>
+
+<?php
+// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	// Could not get the data that should have been sent.
+	die ('niet werken lan!');
+}
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+
+    $stmt->store_result();
+    
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+       
+        if (password_verify($_POST['password'], $password)) {
+          
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+            echo 'Welkom ' . $_SESSION['name'] . '!';
+        } else {
+            header('location: pu.php');
+        }
+    } else {
+        header('location: pu.php');
+    }
+
+	$stmt->close();
+}
+
+?>
