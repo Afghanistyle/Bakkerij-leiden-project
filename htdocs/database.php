23ea3ee94c3d6594266256dd82357b5665931778
Aron <57712842+Afghanistyle@users.noreply.github.com>
2020-03-17 09:17:02 +0100

    Update database.php

--- htdocs/database.php
+++ htdocs/database.php
@@ -1,10 +1,15 @@
-<?php 
-$con = mysqli_connect("localhost","root","","bakkerij_leiden");
+<?php
+session_start();
 
-if(mysqli_connect_errno()){
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'bakkerij_leiden';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-
-mysqli_query($con,"SET NAMES 'utf8' ");
 
 ?>
