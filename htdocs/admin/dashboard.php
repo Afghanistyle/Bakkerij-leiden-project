2224c31d8025f3061b228fc765e861cc37fd5cf1
blueberry217 <58176941+blueberry217@users.noreply.github.com>
2020-03-20 09:17:50 +0100

    htdocs
    update

---
+++ htdocs/admin/dashboard.php
@@ -0,0 +1,36 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link href="CSS/styleDB.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div id="navi">
+        <div id="logout"></div>
+    </div>
+    <p>Welkom, <?=$_SESSION['name']?></p>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+       
