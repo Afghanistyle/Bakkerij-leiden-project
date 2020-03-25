803ada013823806bec4b686d6bb406ad0cfd308c
Aron <57712842+Afghanistyle@users.noreply.github.com>
2020-03-25 14:38:38 +0100

    sdsd

--- htdocs/dashboard.php
+++ htdocs/dashboard.php
@@ -9,25 +9,17 @@ if (!isset($_SESSION['loggedin'])) {
 ?>
 
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Home Page</title>
-		<link href="css/styleDB.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-	</head>
-	<body class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>Website Title</h1>
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
-		<div class="content">
-			<h2>Home Page</h2>
-			<p>Welcome terug, <?=$_SESSION['name']?>!</p>
-		</div>
-	</body>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/styleDB.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	<title>Dashboard</title>
+</head>
+<body>
+	div#
+</body>
 </html>
 
 
