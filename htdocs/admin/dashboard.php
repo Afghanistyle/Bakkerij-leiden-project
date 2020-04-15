fb93b18e6a5e142e99c246d5faa0b893edc5cac3
blueberry217 <58176941+blueberry217@users.noreply.github.com>
2020-04-15 19:26:59 +0200

    htdocs
    dashboard

--- htdocs/admin/dashboard.php
+++ htdocs/admin/dashboard.php
@@ -1,37 +1,53 @@
-<?php include('../includes/database.php'); ?>
-
+<?php
+session_start();
+if (!isset($_SESSION['loggedin'])) {
+header('Location: login.php');
+exit();
+}
+?>
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Home Page</title>
-		<link href="../css/styleDB.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-	</head>
-	<body>
-		<div class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>Dashboard</h1>
-				<a href="../index.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
-		<div class="content">
-			<h2>Beginpagina</h2>
-			<p>Welcome terug!</p>
-			
-		</div>
-	</div>
-	</body>
+<html lang="nl">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Admin Panel - Bakkerij Leiden</title>
+  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico" />
+  <link rel="stylesheet" type="text/css" href="../css/styleDB.css" />
+  <script src="assets/js/main.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" />
+</head>
+<body>
+
+<!-- Navigation. -->
+<div class="loggedin">
+    <nav class="navtop">
+      <div>
+        <h1>Dashboard</h1>
+         <a href="../admin/dashboard.php"><i class="fas fa-user-circle"></i>Home</a>
+        <a href="../admin/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>  
+      </div>
+  </div>
+  <!-- Mobile view navigation -->
+ 
+</header>
+<!-- End of navigation. -->
+
+<!-- Container. -->
+<div class="content">
+  <h2>Dashboard</h2>
+  <div class="box">
+    <!-- Product List. -->
+    <div class="product">
+      <p><a href="products.php"><i class="fas fa-edit"></i> Producten</a> - bekijk uw producten die in de database zijn opgeslagen.</p>
+    </div>
+  </div>
+  <div class="box">
+    <!-- Order list. -->
+    <div class="order">
+      <p><a href="orders.php"><i class="fas fa-edit"></i> Bestellingen</a> - laat zien wie een product heeft besteld.</p>
+    </div>
+  </div>
+   
+</body>
 </html>
-
-
-
-
-
-
-
-
-
-
-       
--- htdocs/admin/login.php
+++ htdocs/admin/login.php
@@ -1,28 +1,36 @@
-<?php include('../includes/database.php'); ?>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Login</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-    <link href="../CSS/style-login.css" rel="stylesheet" type="text/css">
-	</head>
-    <!-- login -->
-		<div class="login">
-      <!-- <div id="shady"></div> -->
-			<h1>Login</h1>
-			<form action="../admin/login-setup.php" method="post">
-				<label for="username">
-					<i class="fas fa-user"></i>
-				</label>
-				<input type="text" name="username" placeholder="Gebruikersnaam" id="username" required>
-				<label for="password">
-					<i class="fas fa-lock"></i>
-				</label>
-				<input type="password" name="password" placeholder="Wachtwoord" id="password" required>
-		<div id="login"><input type="submit" value="Login"></div>
-		<a href="../index.php"><div id="terug"> Terug </div></a>
-			</form>
-      <!-- end login -->
-		</div>
-	</body>
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+  <title>Bakkerij Leiden - Dashboard</title>
+  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico" />
+  <link rel="stylesheet" type="text/css" href="../css/style-login.css" />          
+</head>
+<body>
+
+<!-- Login. -->
+<div class="login">
+  <!-- The heading for login page. -->
+  <div class="heading">
+    <h1>Login</h1>
+    
+  </div>
+  <!-- Form that uses the method post to get data from the database. -->
+  <form action="config.php" method="post">
+    <input class="username" name="username" type="text" maxlength="50" placeholder="Gebruikersnaam" required>
+    <input class="password" name="password" type="password" maxlength="50" placeholder="Wachtwoord" required>
+    <!-- Submit button. Sends all data to config.php. -->
+       <div id="login"><input type="submit" value="Login"></div>
+    <a href="../index.php"><div id="terug"> Terug </div></a>
+  </form>
+  <!-- End of form. -->
+</div>
+<!-- End of login. -->
+
+</body>
 </html>
